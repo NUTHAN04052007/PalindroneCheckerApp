1cb26561d6eb12c57b085c426823096ecdab02a9
NUTHAN04052007 <nuthanvonti2007@gmail.com>
2026-03-02 13:53:33 +0530

    Added UC3 palindrome logic using string reverse

---
+++ App/src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            char ch = stack.pop();
+
+            if (input.charAt(i) != ch) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
