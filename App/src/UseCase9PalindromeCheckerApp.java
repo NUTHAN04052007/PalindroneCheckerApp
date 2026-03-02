ecda8aac2cd6b0604d8ed3965925be90d80b96a0
NUTHAN04052007 <nuthanvonti2007@gmail.com>
2026-03-02 14:09:54 +0530

    Added UC6 Queue + Stack palindrome implementation

---
+++ App/src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive function
+    static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
